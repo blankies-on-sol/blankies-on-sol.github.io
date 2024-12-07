d8e486f4bcfb2a0c4bc83faabca2a037f0454e84
raphastanaël <none>
2024-12-07 15:55:06 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "frog-green": "#5b8e41",
+        "blanket-red": "#e17858"
+      }
+    },
+  },
+  plugins: [],
+}
+
